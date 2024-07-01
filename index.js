6a013522fb6fafaf8883802c250c1181ba99e1ae
Shrusti Vaidya <shrustivaidya2001@gmail.com>
2024-07-01 10:36:51 +0530

    PortNumber changed

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3001
 
 //schema
 const schemaData = mongoose.Schema({
